975babcb565a7e3842a91c085c8a9fcff3477754
Vincent Rabaud <vrabaud@prl1.(none)>
2011-12-14 16:49:19 -0800

    - fix the code to work with the trunk of OR - add the possibility to only submit object_i

--- object_sync.py
+++ object_sync.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('object_recognition')
+import roslib; roslib.load_manifest('object_recognition_core')
 import object_recognition
 from object_recognition import dbtools, models
 import couchdb
@@ -10,7 +10,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Search for an objects in the DB based on tags..')
     parser.add_argument('-t', '--tag', metavar='TAG', dest='tag', type=str, default='',
                        help='Tag to search for.')
-    object_recognition.dbtools.add_db_options(parser)
+    parser.add_argument('--object_ids', help='If set, it overrides the list of object_ids in the config file', default=None)
+    object_recognition.dbtools.add_db_arguments(parser)
     args = parser.parse_args()
     return args
 def meshlab(filename_in, filename_out):
@@ -24,7 +25,14 @@ if __name__ == "__main__":
     args = parse_args()
     couch = couchdb.Server(args.db_root)
     db = dbtools.init_object_databases(couch)
-    results = models.Object.by_object_name(db)
+    if vars(args).get('object_ids', None):
+        results = []
+        object_ids = eval(vars(args).get('object_ids'))
+        for obj in models.Object.all(db):
+            if obj.id in object_ids:
+                results.append(obj)
+    else:
+        results = models.Object.by_object_name(db)
     import os
     if not os.path.exists('scratch'):
         os.makedirs('scratch')
@@ -39,7 +47,7 @@ if __name__ == "__main__":
 
         with open(png, 'w') as imagef:
             imagef.write(image.read())
-        meshes = models.Model.by_object_id_and_mesh(db, key=obj.id, limit=1).rows
+        meshes = models.Model.by_object_id_and['mesh'](db, key=obj.id, limit=1).rows
         if len(meshes) == 0:
             sys.stderr.write("No mesh for object %s, skipping!\n" % (str(obj.id)))
             continue
