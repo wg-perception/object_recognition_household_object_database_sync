23da22a2bda39ddf732802c97a5d6cfb19b4721d
Vincent Rabaud <vrabaud@willowgarage.com>
2012-01-25 21:12:02 -0800

    make sure some parameters are given

--- object_sync.py
+++ object_sync.py
@@ -10,14 +10,16 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Search for an objects in the DB based on tags..')
     parser.add_argument('-t', '--tag', metavar='TAG', dest='tag', type=str, default='',
                        help='Tag to search for.')
-    parser.add_argument('-m', '--method', metavar='METHOD', dest='method', type=str, default='',
-                       help='Method used to compute the model.')
-    parser.add_argument('--object_ids', help='If set, it overrides the list of object_ids in the config file', default=None)
+    parser.add_argument('-m', '--method', metavar='METHOD', dest='method', type=str,
+                       help='Method used to compute the model.', required=True)
+    parser.add_argument('--object_ids', help='The list of object_ids to synchronize', required=True)
     object_recognition.dbtools.add_db_arguments(parser)
     args = parser.parse_args()
     return args
+
 def meshlab(filename_in, filename_out):
     subprocess.check_call((['meshlabserver', '-i', filename_in, '-o', filename_out]))
+
 def househould_insert(thumbnail, meshfile, method, obj):
     call = ['rosrun', 'object_recognition_household_object_database_sync', 'insert_model', '-G', meshfile, '-T', thumbnail, '-N', obj.object_name, '-D', obj.description, '-I', str(obj.id), '-M', method]
     call += list(obj.tags)
@@ -60,4 +62,3 @@ if __name__ == "__main__":
             meshf.write(mesh.read())
         meshlab(mesh_stl, mesh_ply)
         househould_insert(thumbnail=png, meshfile=mesh_ply, method=vars(args).get('method'), obj=obj)
-
